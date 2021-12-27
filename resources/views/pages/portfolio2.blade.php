e61de01cef8cca692af39f7ea0abdf2241ec3cc9
fateme <fatemem1990@yahoo.com>
2021-12-27 20:24:21 +0330

    add portfolio2.blade.php

---
+++ resources/views/pages/portfolio2.blade.php
@@ -0,0 +1,222 @@
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<!--Hero-->
+<section class="section section-hero section-hero-15 section-wave section-wave-1 overlay-color overlay-pattern">
+    <span class="bg-color bg-dark-30"></span>
+    <span class="bg-pattern bg-pattern-3"></span>
+    <div class="hero-frame">
+        <img src="/afracode/assets/images/demo/frame.png" alt="Alternative">
+        <div class="frame-lcd overlay-color">
+            <div class="el-slider">
+                <div class="el-swiper-hero-demo swiper-container">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="el-item">
+                                <img src="/afracode/assets/images/demo/hero-1.jpg" alt="--Alternative--">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="el-item">
+                                <img src="/afracode/assets/images/demo/hero-2.jpg" alt="--Alternative--">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="el-item">
+                                <img src="/afracode/assets/images/demo/hero-3.jpg" alt="--Alternative--">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </div>
+            <span class="bg-color bg-dark-30"></span>
+        </div>
+    </div>
+    <div class="display-center">
+        <div class="container">
+            <div class="hero-content text-left">
+                <h1 class="text-light">A better way to build eCommerce websites.</h1>
+                <p class="text-light">Create your dream site with Orwell HTML Template.</p>
+                <a href="http://demo.afracode.com/orwell/home-1.html" class="button button-md button-primary">
+                    <span class="text">LIVE DEMO</span>
+                </a>
+                <a href="#" class="button button-md button-secondary">
+                    <span class="text">PURCHASE</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="hero-icons">
+        <div class="container">
+            <p class="text-light">Compatible with:</p>
+            <ul>
+                <li class="overlay-image">
+                    <img src="/afracode/assets/images/demo/icon-1.svg" alt="--Feature--">
+                </li>
+                <li class="overlay-image">
+                    <img src="/afracode/assets/images/demo/icon-2.svg" alt="--Feature--">
+                </li>
+                <li class="overlay-image">
+                    <img src="/afracode/assets/images/demo/icon-3.svg" alt="--Feature--">
+                </li>
+                <li class="overlay-image">
+                    <img src="/afracode/assets/images/demo/icon-4.svg" alt="--Feature--">
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--/-->
+<!--Features-->
+<section class="section-features section-features-16 pt-space">
+    <div class="container">
+        <header class="el-heading el-heading-1 mb-space center">
+            <span class="label">MAIN FEATURES</span>
+            <h2>Why Choose Us?</h2>
+        </header>
+        <div class="row row-lg">
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-1.png" alt="Features">
+                    </div>
+                    <h4>Fully Responsive Layout</h4>
+                    <p>Responsive web design is an approach to web design that makes web pages render well on a variety of devices screen sizes.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-2.png" alt="Features">
+                    </div>
+                    <h4>SEO Friendly</h4>
+                    <p>Our theme is SEO friendly that aligns with your digital marketing strategies to bring more organic traffic to your website.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-3.png" alt="Features">
+                    </div>
+                    <h4>Faster Loading</h4>
+                    <p>Moreover, your website speed is very important for your SEO. If your website loads fast, your ranking in search engines will be higher.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-4.png" alt="Features">
+                    </div>
+                    <h4>Google Font</h4>
+                    <p>A vast collection of Google fonts are integrated with the theme. You can use any of them that goes with your branding.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-5.png" alt="Features">
+                    </div>
+                    <h4>Creative Design</h4>
+                    <p>[Name] is a bold theme that organizing content to follow natural eyes, striking balance between beautiful and visual hierarchy.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-6.png" alt="Features">
+                    </div>
+                    <h4>Lifetime Update</h4>
+                    <p>Purchase our theme only once and get lifetime updates. We keep updating our products to stay up to date with latest trends and technology.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-7.png" alt="Features">
+                    </div>
+                    <h4>Mobile Optimized</h4>
+                    <p>A mobile optimized site is a far more advanced website. Mobile optimized means that the site will reformat itself for a list of devices.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-8.png" alt="Features">
+                    </div>
+                    <h4>Font Awesome</h4>
+                    <p>This theme comes with high quality icons from FontAwesome. All the icons are font based and ready to match the quality of any HQ screen.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-12 col-lg-6 col-xl-4">
+                <div class="el-card el-feature el-feature-16">
+                    <div class="icon">
+                        <img src="/afracode/assets/images/icons/feature-9.png" alt="Features">
+                    </div>
+                    <h4>Ready for Deployment</h4>
+                    <p>We have made the deployment process clean and simple. You can deploy the template with Now.sh.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/-->
+<!--Plan-->
+<section class="section section-plan section-plan-1 pt-space pb-space">
+    <div class="container">
+        <header class="el-heading el-heading-1 center pb-space">
+            <span class="label mb-5">PLANS</span>
+            <h2>Simple, Straight Pricing</h2>
+        </header>
+        <div class="row row-lg justify-content-center">
+            <div class="col-md col-lg-4">
+                <div class="el-card el-plan el-plan-1">
+                    <div class="el-plan-head">
+                        <div class="price">
+                            <span class="sign">$</span>
+                            <span class="value">18</span>
+                        </div>
+                        <span class="title">Standard License</span>
+                    </div>
+                    <div class="el-plan-body">
+                        <ul>
+                            <li>1 end product</li>
+                            <li>Use for personal or a client</li>
+                            <li>Use in a free end product</li>
+                            <li>6 months technical support</li>
+                        </ul>
+                        <a href="#" class="button button-md button-warning" target="_blank">Purchase now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md col-lg-4">
+                <div class="el-card el-plan el-plan-1 bg-primary text-light">
+                    <div class="el-plan-head">
+                        <div class="price">
+                            <span class="sign">$</span>
+                            <span class="value">570</span>
+                        </div>
+                        <span class="title">Extended License</span>
+                    </div>
+                    <div class="el-plan-body">
+                        <ul>
+                            <li>1 end product</li>
+                            <li>Use for personal or a client</li>
+                            <li>Use in a free end product</li>
+                            <li>Use in an end product that is <strong>sold</strong></li>
+                            <li>6 months technical support</li>
+                        </ul>
+                        <a href="#" class="button button-md button-light" target="_blank">Purchase now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/-->
+
+
+@endsection
