0daa413b47eca0b200a4ea31782fb6aecb0c4dd6
majid salehi <majidsalehi67@gmail.com>
2021-12-28 19:15:02 +0330

    add portfolio controller

---
+++ app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Portfolio;
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+      $query=Portfolio::get();
+      return view('portfolio.index',['items'=>$query]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('portfolio.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $inputs=$request->only('title','price','body','image_path');
+        Portfolio::create($inputs);
+        return redirect('portfolio');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $query=Portfolio::find($id);
+        return view('portfolio.show',['items'=>$query]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $query=Portfolio::where('id',$id)->first();
+        return view('portfolio.edit',['items'=>$query]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $query=$request->only(['title','price','body','image_path']);
+        Portfolio::where('id',$id)->update($query);
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        Portfolio::query()->where('id',$id)->delete();
+        return back();
+    }
+}
