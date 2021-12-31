25079153193b0022842985d14a2c67d05c813b2d
majid salehi <majidsalehi67@gmail.com>
2021-12-31 18:16:12 +0330

    add blog news controller

---
+++ app/Http/Controllers/BlogNewsController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogNews;
+use App\Models\Portfolio;
+use Illuminate\Http\Request;
+
+class BlogNewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $query=BlogNews::get();
+        return view('blog.index',['items'=>$query]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('blog.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $inputs=$request->only('title','body','image_path','author_image','author_name');
+        BlogNews::create($inputs);
+        return redirect('blog');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $query=BlogNews::find($id);
+        return view('blog.show',['items'=>$query]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $query=BlogNews::where('id',$id)->first();
+        return view('blog.edit',['items'=>$query]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $query=$request->only(['title','body','image_path','author_image','author_name']);
+        BlogNews::where('id',$id)->update($query);
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        BlogNews::query()->where('id',$id)->delete();
+        return back();
+    }
+}
