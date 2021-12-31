a9fc41d8a35b633b5bb449d7fb3e4b26b17eb13e
majid salehi <majidsalehi67@gmail.com>
2021-12-31 17:42:34 +0330

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\BlogNews;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([ServiceSeeder::class]);
         $this->call([PortfolioSeeder::class]);
+        $this->call([BlogNewsSeeder::class]);
 
 
     }
