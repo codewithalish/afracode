f90fcdfe9c68ec148a580b5b958dddf0575af843
fateme <fatemem1990@yahoo.com>
2022-01-03 20:50:58 +0330

    edit web.php

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,6 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\LoginController;
 use App\Http\Controllers\adminController;
 use Illuminate\Support\Facades\Hash;
-/*
-|--------------------------------------------------------------------------
-| Dev
-|--------------------------------------------------------------------------
-|
-|
-*/
-
-
-Route::view('/test/welcome','pages.welcome');
-Route::view('/test/blog','pages.blog');
-Route::view('/test/contact','pages.contact');
-Route::view('/test/about','pages.about');
-Route::view('/test/portfolio2','pages.portfolio2');
-Route::view('/test/portfolio','pages.portfolio');
-
-Route::get('/test/password/{pass}',function ($pass){
-    return Hash::make($pass);
-});
 
 
 /*
@@ -35,8 +16,27 @@ Route::get('/test/password/{pass}',function ($pass){
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 //Route::get('/', [\App\Http\Controllers\SeviceController::class, 'welcome']);
+
+
+/*
+|--------------------------------------------------------------------------
+| Dev
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+Route::view('/test/welcome','pages.welcome');
+Route::view('/test/blog','pages.blog');
+Route::view('/test/contact','pages.contact');
+Route::view('/test/about','pages.about');
+Route::view('/test/portfolio2','pages.portfolio2');
+Route::view('/test/portfolio','pages.portfolio');
+Route::get('/test/password/{pass}',function ($pass){
+    return Hash::make($pass);
+});
+
 
 /*
 |--------------------------------------------------------------------------
@@ -51,4 +51,6 @@ Route::get('/test/password/{pass}',function ($pass){
 Route::get('/admin/login', [LoginController::class,'login']);
 Route::post('/admin/login', [LoginController::class,'checkLogin']);
 Route::get('/admin/dashboard', [adminController::class,'dashboard']);
+Route::get('/admin/register', [LoginController::class,'create']);
+Route::post('/admin/register', [LoginController::class,'register']);
 
