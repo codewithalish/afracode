b5a1d913726e5f5e8d543b22d8ef90151edc621e
majid salehi <majidsalehi67@gmail.com>
2022-01-05 17:00:47 +0330

    fix bug

--- database/migrations/2022_01_05_163139_create_message_table.php
+++ database/migrations/2022_01_05_163139_create_message_table.php
@@ -13,7 +13,7 @@ class CreateMessageTable extends Migration
      */
     public function up()
     {
-        Schema::create('message', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
