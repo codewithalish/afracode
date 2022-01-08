84614e1df983359e4208e274159db2ee4c29ac02
fateme <fatemem1990@yahoo.com>
2022-01-08 18:37:52 +0330

    drop column price from posts table

--- database/migrations/2022_01_04_195443_create_posts_table.php
+++ database/migrations/2022_01_04_195443_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('image_path')->nullable();
             $table->string('body');
             $table->text('details');
-            $table->bigInteger('price')->nullable();
+            $table->bigInteger('price')->nullable(  );
             $table->timestamps();
         });
     }
